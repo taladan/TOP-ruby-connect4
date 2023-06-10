88848c629a296ca20022382449041f51ae2e370b
Jamie Crosby <taladan@gmail.com>
2023-06-10 08:15:41 -0400

    test: update spec
    Display is mostly just print/puts/gets stuff which is well tested
    already in ruby's core.  There is a little looping logic in
    the print_board, but that's an implementation detail that doesn't
    really need to be tested.

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -5,21 +5,13 @@ describe Display do
   subject(:display) { Display.new }
 
   describe '#prompt_for_name' do
-    context "When entering the text 'Dean'" do
-      before do
-        allow(display).to receive(gets.chomp).and_return('Dean')
-      end
-
-      it "returns the text 'Dean'" do
-        expect(display.prompt_for_name('Player 1')).to eq('Dean')
-      end
-    end
+    # Only puts and gets, doesn't need to be tested
   end
 
   describe '#prompt_for_play' do
-    context do
-      xit do
-      end
-    end
+    # Only puts and gets, doesn't need to be tested
   end
+  
+  describe '#print_board' do
+    # Only prints info to screen, doesn't need to be tested
 end
